8d5e250bc70d76572a0ddc4634bc33c4bf9dc4a7
Ely Lucas <ely@meta-tek.net>
2020-02-08 06:20:12 -0700

    rooms controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { RoomsRepository } from './data/repositories/rooms.repository';
 import { SessionsRepository } from './data/repositories/sessions.repository';
 import { SpeakersRepository } from './data/repositories/speakers.repository';
+import { RoomsController } from './rooms/rooms.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, RoomsController],
   providers: [AppService, RoomsRepository, SpeakersRepository, SessionsRepository],
 })
 export class AppModule {}
