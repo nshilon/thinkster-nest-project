f3b452ed4baf826f384a62eaad59a9344bc07e0c
Ely Lucas <ely@meta-tek.net>
2020-02-13 05:22:59 -0700

    updating comment

--- src/speakers/speakers.controller.ts
+++ src/speakers/speakers.controller.ts
@@ -4,6 +4,8 @@ import { Controller } from '@nestjs/common';
 // We want to make a request to /speakers to get the list of speakers
 // but the following controller is missing a couple of items to do so.
 // Fix the controller below to be able to make a request to /speakers.
+//
+// Solution Branch: exercise-controller-solution
 
 @Controller('')
 export class SpeakersController {
