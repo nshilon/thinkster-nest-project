b4a58413cffc1f121900ad90f622cc275f5a378c
Ely Lucas <ely@meta-tek.net>
2020-02-14 05:46:04 -0700

    pre

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RoomsRepository } from './data/repositories/rooms.repository';
-import { SessionsRepository } from './data/repositories/sessions.repository';
-import { SpeakersRepository } from './data/repositories/speakers.repository';
 import { RoomsController } from './rooms/rooms.controller';
 import { SpeakersController } from './speakers/speakers.controller';
+import { RoomsRepository } from './data/repositories/rooms.repository';
 
 @Module({
   imports: [],
   controllers: [AppController, RoomsController, SpeakersController],
-  providers: [AppService, RoomsRepository, SpeakersRepository, SessionsRepository],
+  providers: [AppService, RoomsRepository],
 })
 export class AppModule {}
--- src/speakers/speakers.controller.ts
+++ src/speakers/speakers.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { SpeakersRepository } from 'src/data/repositories/speakers.repository';
+
+// Exercise:
+// While this code works, it is tightly coupling the SpeakersRepository to the controller.
+// Fix that by using dependency injection instead.
+//
+// Solution Branch: exercise-controller-solution
 
 @Controller('speakers')
 export class SpeakersController {
 
+
   @Get()
   getList() {
-    return [
-      { name: 'Amanda' },
-      { name: 'Ben' }
-    ]
+    const repo = new SpeakersRepository();
+    return repo.getAll();
   }
 }
