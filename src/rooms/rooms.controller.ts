4aa5daa565590922a1071cb36ce14be7929473f2
Ely Lucas <ely@meta-tek.net>
2020-02-15 15:59:54 -0700

    undoing

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { RoomsRepository } from 'src/data/repositories/rooms.repository';
 
 @Controller('rooms')
