c8e953986208ca5d613f302afc85117e9f0be655
Ely Lucas <ely@meta-tek.net>
2020-02-15 15:53:46 -0700

    undoing

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -6,14 +6,8 @@ export class RoomsController {
 
   constructor(private roomsRepository: RoomsRepository) {}
 
-  @Get(':id')
-  get(@Param('id') idStr: string) {
-    const id = parseInt(idStr, 10);
-    return this.roomsRepository.get(id);
-  }
-
   @Get()
-  getList(@Query('name') name?: string) {
-    return this.roomsRepository.getAll({name});
+  getList() {
+    return this.roomsRepository.getAll();
   }
 }
