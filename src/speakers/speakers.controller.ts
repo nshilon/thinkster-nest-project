fdc8ccce92ab75502e79f617a895eae99018cc6c
Ely Lucas <ely@meta-tek.net>
2020-02-16 06:57:24 -0700

    get single

--- src/speakers/speakers.controller.ts
+++ src/speakers/speakers.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { SpeakersRepository } from 'src/data/repositories/speakers.repository';
 
 @Controller('speakers')
 export class SpeakersController {
 
   constructor(private speakersRepository: SpeakersRepository) { }
+
+  @Get(':id')
+  get(@Param('id') idStr: string) {
+    const id = parseInt(idStr, 10);
+    return this.speakersRepository.get(id);
+  }
 
   @Get()
   getList() {
