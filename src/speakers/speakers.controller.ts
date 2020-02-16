4338f1425412f5fcdbcb110d78b7527fc3c6b6c3
Ely Lucas <ely@meta-tek.net>
2020-02-15 17:58:15 -0700

    exercise prep

--- src/speakers/speakers.controller.ts
+++ src/speakers/speakers.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { SpeakersRepository } from 'src/data/repositories/speakers.repository';
+
+// Exercise:
+// The get method below as two errors in it that is keeping the request
+// from succeeding. Fix the errors and make a request to speakers/1 to get
+// back the speaker
 
 @Controller('speakers')
 export class SpeakersController {
 
   constructor(private speakersRepository: SpeakersRepository) { }
+
+  @Get('id')
+  get(idStr: string) {
+    const id = parseInt(idStr, 10);
+    return this.speakersRepository.get(id);
+  }
 
   @Get()
   getList() {
