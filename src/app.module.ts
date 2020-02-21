1ffd51d871b4610d3773d05151aeba36793a2cc7
Ely Lucas <ely@meta-tek.net>
2020-02-21 10:55:07 -0700

    providers exercise solution

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { RoomsController } from './rooms/rooms.controller';
 import { SpeakersController } from './speakers/speakers.controller';
 import { RoomsRepository } from './data/repositories/rooms.repository';
+import { SpeakersRepository } from './data/repositories/speakers.repository';
 
 @Module({
   imports: [],
   controllers: [AppController, RoomsController, SpeakersController],
-  providers: [AppService, RoomsRepository],
+  providers: [AppService, RoomsRepository, SpeakersRepository],
 })
 export class AppModule {}
--- src/speakers/speakers.controller.ts
+++ src/speakers/speakers.controller.ts
@@ -10,10 +10,11 @@ import { SpeakersRepository } from 'src/data/repositories/speakers.repository';
 @Controller('speakers')
 export class SpeakersController {
 
+  constructor(private speakersRepository: SpeakersRepository) {}
+
 
   @Get()
   getList() {
-    const repo = new SpeakersRepository();
-    return repo.getAll();
+    return this.speakersRepository.getAll();
   }
 }
