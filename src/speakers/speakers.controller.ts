138f0a5e5e5fe50f3dea4073262be940e3ed3bd0
Ely Lucas <ely@meta-tek.net>
2020-02-22 12:41:37 -0700

    section prep

--- src/speakers/speakers.controller.ts
+++ src/speakers/speakers.controller.ts
@@ -2,26 +2,10 @@ import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
 import { SpeakersRepository } from 'src/data/repositories/speakers.repository';
 import { SpeakerEntity } from 'src/data/speaker.entity';
 
-// Exercise:
-// The create method down below is missing the required Nest decorators
-// Add them, the make a POST request to /speakers in Postman with the following request body:
-// {
-// 	"name": "Billy Bob", 
-// 	"hasSpokeBefore": false
-// }
-// Verify you get a 201 status code and that you can view the new speaker when
-// making a GET request to /speakers
-
 @Controller('speakers')
 export class SpeakersController {
 
   constructor(private speakersRepository: SpeakersRepository) { }
-
-  create(speaker: unknown) {
-    const speakerEntity = Object.assign(new SpeakerEntity(), speaker);
-    speakerEntity.createdBy = 'admin';
-    return this.speakersRepository.create(speakerEntity);
-  }
 
   @Get(':id')
   get(@Param('id') idStr: string) {
@@ -40,6 +24,11 @@ export class SpeakersController {
     return this.speakersRepository.getAll({ name, hasSpokenBefore });
   }
 
-  
+  @Post()
+  create(@Body() speaker: unknown) {
+    const speakerEntity = Object.assign(new SpeakerEntity(), speaker);
+    speakerEntity.createdBy = 'admin';
+    return this.speakersRepository.create(speakerEntity);
+  }  
 
 }
