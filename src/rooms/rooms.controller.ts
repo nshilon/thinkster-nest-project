a53e5a76716c329c22b67eec4a25509cdaa97d48
Ely Lucas <ely@meta-tek.net>
2020-02-22 09:56:49 -0700

    rooms cud

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, Post, Body, Put, Delete } from '@nestjs/common';
 import { RoomsRepository } from 'src/data/repositories/rooms.repository';
+import { RoomEntity } from 'src/data/room.entity';
 
 @Controller('rooms')
 export class RoomsController {
@@ -17,4 +18,25 @@ export class RoomsController {
     const capacity = parseInt(capacityStr, 10);
     return this.roomsRepository.getAll({ name, capacity });
   }
+
+  @Post()
+  create(@Body() room: unknown) {
+    const roomEntity = Object.assign(new RoomEntity(), room);
+    roomEntity.createdBy = 'admin';
+    return this.roomsRepository.create(roomEntity);
+  }
+
+  @Put(':id')
+  update(@Param('id') idStr: string, @Body() room: unknown) {
+    const id = parseInt(idStr, 10);
+    const roomEntity = Object.assign(new RoomEntity(), room);
+    return this.roomsRepository.update(id, roomEntity);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') idStr: string) {
+    const id = parseInt(idStr, 10);
+    return this.roomsRepository.delete(id);
+  } 
+  
 }
