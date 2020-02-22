fee4876b6e2651d6fbb68a1a408bb9991e260183
Ely Lucas <ely@meta-tek.net>
2020-02-22 12:38:04 -0700

    post room controller

--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, Post, Body } from '@nestjs/common';
 import { RoomsRepository } from 'src/data/repositories/rooms.repository';
+import { RoomEntity } from 'src/data/room.entity';
 
 @Controller('rooms')
 export class RoomsController {
@@ -16,5 +17,12 @@ export class RoomsController {
   getList(@Query('name') name: string, @Query('capacity') capacityStr: string) {
     const capacity = parseInt(capacityStr, 10);
     return this.roomsRepository.getAll({ name, capacity });
+  }
+
+  @Post()
+  create(@Body() room: unknown) {
+    const roomEntity = Object.assign(new RoomEntity(), room);
+    roomEntity.createdBy = 'admin';
+    return this.roomsRepository.create(roomEntity);
   }
 }
