600e90c89e04c6b47a301ad823893b20c0ac8065
Ely Lucas <ely@meta-tek.net>
2020-02-25 05:15:50 -0700

    exercise pipe prep

--- src/util/convert.pipe.ts
+++ src/util/convert.pipe.ts
@@ -1,5 +1,12 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 
+// Exercise:
+// Move the string to boolean conversion logic from the speakers controller into
+// the pipe below. Make sure to test if the metatype is a Boolean and run the logic if so.
+// Update the hasSpokenBefore param to getList to be a boolean.
+// Once done, make a request to /speakers?hasSpokenBefore=true and make sure the request
+// still returns the appropriate results.
+
 @Injectable()
 export class ConvertPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
@@ -10,6 +17,8 @@ export class ConvertPipe implements PipeTransform {
     if(metadata.metatype === Number) {
       return parseInt(value, 10);
     }
+
+    //Todo: Add boolean convert logic here
 
     return value;
   }
