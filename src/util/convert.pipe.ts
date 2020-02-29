76bd3c91be8cef7e77a92a2a8d72ebe3acf7cd30
Ely Lucas <ely@meta-tek.net>
2020-02-29 09:55:51 -0700

    fix null check in convert pipe

--- src/util/convert.pipe.ts
+++ src/util/convert.pipe.ts
@@ -4,7 +4,7 @@ import { plainToClass } from 'class-transformer';
 @Injectable()
 export class ConvertPipe implements PipeTransform {
   transform(value: any, metadata: ArgumentMetadata) {
-    if (typeof value == null) {
+    if (value == null) {
       return undefined;
     }
 
