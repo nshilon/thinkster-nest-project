39dce160bbbfef1c71ab6b9ba7d8de88b972dce5
Ely Lucas <ely@meta-tek.net>
2020-03-01 14:06:08 -0700

    prep

--- src/data/speaker.entity.ts
+++ src/data/speaker.entity.ts
@@ -3,6 +3,7 @@
 // Add decorators from class-validator to make sure that:
 // 1) There is a value provided for name, that it is a string, and it is not empty
 // 2) There is a value provided for hasSpokeBefore, and that it is a boolean
+// 3) IF there is a value passed in for bio, make sure its a non-empty string (hint: you will need a new decorator not discussed yet)
 // Run POST and PUT requests to /speakers, /speakers/:id to make sure the validation logic is working properly
 
 export class SpeakerEntity {
