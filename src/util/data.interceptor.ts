5995347df6d71426e93dd8603eaffea5207bb8ba
Ely Lucas <ely@meta-tek.net>
2020-08-12 17:40:27 -0600

    exercise instructions

--- src/util/data.interceptor.ts
+++ src/util/data.interceptor.ts
@@ -2,6 +2,33 @@ import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nes
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+// Exercise:
+// Update the result that gets returned to have a meta field that will contain meta data 
+// about the request. 
+// If the result coming back from the controller is an array, move the array
+// info to the meta data field (page, pageSize, etc...), and set the data field to the
+// value of the list array.
+// If the result is a single object, set that object to the data field.
+// Bonus: Set the status code of the response to meta.statusCode.
+// Once done, make a request to /rooms and verify the object is similar to:
+// {
+//   "id": "cvaahwyw6",
+//   "meta": {
+//     "statusCode": 200,
+//     "page": 1,
+//     "pageSize": 10,
+//     "totalPages": 1,
+//     "total": 10
+//   }
+//   "data": [...];
+// }
+// And then make a request to /rooms/1 and verify the result is like:
+//   "id": "cvaahwyw6",
+//   "meta": {
+//     "statusCode": 200
+//   }
+//   "data": {...};
+// }
 
 @Injectable()
 export class DataInterceptor implements NestInterceptor {
